0f7753dad933ed678c0bc0760698e278b47d2e06
Mahesh Chand <mahesh.kndpl@gmail.com>
2025-10-19 13:50:42 +0530

    Optimal approach for buy and sell stock

--- src/arrays/medium/BuyAndSellStock.java
+++ src/arrays/medium/BuyAndSellStock.java
@@ -14,10 +14,28 @@ public class BuyAndSellStock {
         return max;
     }
 
+    /*
+         Optimized approach
+     */
+    public static int maxProfitOptimal(int[] prices) {
+        int minimumCost = prices[0], maxProfit = 0;
+        for (int i = 1; i < prices.length; i++) {
+                if (prices[i] < minimumCost) {
+                    minimumCost = prices[i];
+                }
+
+            maxProfit = Math.max(prices[i] - minimumCost, maxProfit);
+        }
+        return maxProfit;
+    }
+
     public static void main(String[] args) {
         int[] prices = {7,1,5,3,6,4};
-        int[] prices2 = {7,6,4,3,1};
+        int[] prices2 = {1, 2};
         System.out.println("Max Profit: " + maxProfit(prices));
         System.out.println("Max Profit: " + maxProfit(prices2));
+
+        System.out.println("Max Profit: " + maxProfitOptimal(prices));
+        System.out.println("Max Profit: " + maxProfitOptimal(prices2));
     }
 }
