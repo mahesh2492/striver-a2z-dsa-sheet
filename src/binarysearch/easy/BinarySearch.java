04fd3e2a1b206d96c4e63e5f12dafe9899df62ea
Mahesh Chand <mahesh.kndpl@gmail.com>
2025-10-22 16:22:33 +0530

    solve binary search problem

---
+++ src/binarysearch/easy/BinarySearch.java
@@ -0,0 +1,29 @@
+package binarysearch.easy;
+
+public class BinarySearch {
+
+    public static int search(int[] nums, int target) {
+      int low = 0, high = nums.length - 1;
+       while(low <= high) {
+           int mid = low + (high - low) / 2;
+
+           if(nums[mid] == target) {
+               return mid;
+           } else if(target > nums[mid]) {
+               low = mid + 1;
+           } else {
+               high = mid - 1;
+           }
+       }
+
+      return -1;
+    }
+
+    public static void main(String[] args) {
+      int[] arr = {-1,0,3,5,9,12};
+      int[] arr2 = {-1,0,3,5,9,12};
+
+      System.out.println("Index of target element: " + search(arr, 9));
+      System.out.println("Index of target element: " + search(arr2, 0));
+    }
+}
