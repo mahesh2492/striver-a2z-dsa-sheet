1bdf12a7742b35742d405b744be751150c556c20
Mahesh Chand <mahesh.kndpl@gmail.com>
2025-10-24 18:55:31 +0530

    Solve ceil in sorted array

---
+++ src/binarysearch/easy/CeilInSortedArray.java
@@ -0,0 +1,27 @@
+package binarysearch.easy;
+
+public class CeilInSortedArray {
+
+    public static int findCeil(int[] arr, int target) {
+        int ans = arr.length;
+        if(arr[ans - 1] < target) {
+            return -1;
+        }
+
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] > target) {
+                ans = i;
+                break;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+       int[] arr = {1, 2, 8, 10, 11, 12, 19};
+
+        System.out.println("Ceiling in Sorted Array: " + findCeil(arr, 5));
+        System.out.println("Ceiling in Sorted Array: " + findCeil(arr, 20));
+        System.out.println("Ceiling in Sorted Array: " + findCeil(arr, 0));
+    }
+}
