f87a864b63dbb8feef331cf2e329c6270eb6be34
Mahesh Chand <mahesh.kndpl@gmail.com>
2025-10-25 11:37:30 +0530

    Solve search first and last index of target in sorted array

--- src/binarysearch/easy/SearchFirstAndLast.java
+++ src/binarysearch/easy/SearchFirstAndLast.java
@@ -20,6 +20,39 @@ public class SearchFirstAndLast {
         return new int[]{first, last};
     }
 
+    public static int[] searchRangeUsingBS(int[] nums, int target) {
+        int[] ans = {-1, -1};
+
+        ans[0] = search(nums, target, true);
+        if(ans[0] != -1) {
+            ans[1] = search(nums, target, false);
+        }
+        return ans;
+    }
+
+    static int search(int[] nums, int target, boolean findStartIndex) {
+        int low = 0, high = nums.length - 1;
+        int ans = -1;
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if(target > nums[mid]) {
+                low = mid + 1;
+            } else if (target < nums[mid]) {
+                high = mid - 1;
+            } else {
+                ans = mid;
+                if(findStartIndex) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            }
+        }
+
+        return ans;
+    }
+
     public static void main(String[] args) {
         int[] arr = {5,7,7,8,8,10};
         int[] arr2 = {1};
@@ -27,5 +60,10 @@ public class SearchFirstAndLast {
         System.out.println("First and Last index of 6: " + Arrays.toString(searchRange(arr, 6)));
         System.out.println("First and Last index of 0: " + Arrays.toString(searchRange(arr, 0)));
         System.out.println("First and Last index of 1: " + Arrays.toString(searchRange(arr2, 1)));
+
+        System.out.println("First and Last index of 8: " + Arrays.toString(searchRangeUsingBS(arr, 8)));
+        System.out.println("First and Last index of 6: " + Arrays.toString(searchRangeUsingBS(arr, 6)));
+        System.out.println("First and Last index of 0: " + Arrays.toString(searchRangeUsingBS(arr, 0)));
+        System.out.println("First and Last index of 1: " + Arrays.toString(searchRangeUsingBS(arr2, 1)));
     }
 }
