32583c4823b228adb69be654f3d1ff10fa0d775a
Mahesh Chand <mahesh.kndpl@gmail.com>
2026-06-19 22:44:32 +0530

    Solve set matrix zero

---
+++ src/arrays/medium/SetMatrixZero.java
@@ -0,0 +1,58 @@
+package arrays.medium;
+
+public class SetMatrixZero {
+
+    /*
+       Time complexity - O(M * N)
+       Space complexity - O(M + N)
+
+     */
+    public static void setZeroes(int[][] matrix) {
+        boolean[] row = new boolean[matrix.length];
+        boolean[] col = new boolean[matrix[0].length];
+
+        for(int i = 0; i < matrix.length; i++) {
+            for(int j = 0; j < matrix[i].length; j++) {
+                if(matrix[i][j] == 0) {
+                    row[i] = true;
+                    col[j] = true;
+                }
+            }
+        }
+
+        for(int i = 0; i < matrix.length; i++) {
+            for(int j = 0; j < matrix[i].length; j++) {
+                if(row[i] || col[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1, 1, 1},
+                {1, 0, 1},
+                {1, 1, 1}
+        };
+
+        for(int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        setZeroes(matrix);
+
+        System.out.println("Matrix after setting zeroes");
+        for(int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
